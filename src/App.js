5ce87fb5050b535774129e839055a3bb0290c1a2
Benoît Hubert <benoithubert@gmail.com>
2019-10-04 09:40:28 +0200

    Add an image

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <p>It is cleaner now.</p>
+      <p>Let's mess it up again. <img src="https://cdn-media.rtl.fr/cache/yzS24VHVNJXqbBR2ZJuMPA/880v587-0/online/image/2015/0309/7776940649_heath-ledger-est-le-dernier-a-avoir-interprete-le-joker-au-cinema-dans-the-dark-knight-2008.jpeg" /></p>
     </div>
   );
 }
